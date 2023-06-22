5c869edeb520dfaca57b03d1efebef354758b02b
unknown <kainesv86@gmail.com>
2023-06-22 09:53:44 +0700

    Style: minor styles navigator

--- App.js
+++ App.js
@@ -5,39 +5,74 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Icon } from "react-native-elements";
 import FavoritesScreen from "./screens/FavoritesScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Orchid List" component={HomeScreen} />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 const HomeStack = () => (
-    <Stack.Navigator>
-        <Stack.Screen name="Orchid List" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
-    </Stack.Navigator>
+  <Stack.Navigator>
+    <Stack.Screen name="Orchid List" component={HomeScreen} />
+    <Stack.Screen name="Detail" component={DetailScreen} />
+  </Stack.Navigator>
+);
+
+const HomeTab = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Orchid List" component={HomeScreen} />
+    <Tab.Screen name="Favorites" component={FavoritesScreen} />
+  </Tab.Navigator>
 );
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen
-                    name="HomeStack"
-                    component={HomeStack}
-                    options={{
-                        headerShown: false,
-                        tabBarLabel: "Home",
-                        tabBarIcon: ({ color, size }) => <Icon name="home" type="font-awesome" color={color} size={size} />,
-                    }}
-                />
-                <Tab.Screen
-                    name="Favorites"
-                    component={FavoritesScreen}
-                    options={{
-                        tabBarLabel: "Favorites",
-                        tabBarIcon: ({ color, size }) => <Icon name="heart" type="font-awesome" color={color} size={size} />,
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        {/* <Tab.Screen
+          name="MyDrawer"
+          component={MyDrawer}
+          options={{
+            headerShown: false,
+          }}
+        /> */}
+
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            headerShown: false,
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" type="font-awesome" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={FavoritesScreen}
+          options={{
+            tabBarLabel: "Favorites",
+            tabBarIcon: ({ color, size }) => (
+              <Icon
+                name="heart"
+                type="font-awesome"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
